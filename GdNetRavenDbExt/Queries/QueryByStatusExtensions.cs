dbd2750d457af9aa3627c9cc46c5fec7a9e8f807
juanonsoftware <juanonsoftware@gmail.com>
2026-04-03 18:35:10 +0700

    Adding new query extensions

---
+++ GdNetRavenDbExt/Queries/QueryByStatusExtensions.cs
@@ -0,0 +1,47 @@
+﻿using GdNetDDD.Entities;
+using GdNetDDD.Errors;
+using Raven.Client.Documents;
+using Raven.Client.Documents.Linq;
+
+namespace GdNetRavenDbExt.Queries;
+
+using GdNetDDD.Queries;
+
+public static class QueryByStatusExtensions
+{
+    public static IRavenQueryable<TEntity> FilterByStatus<TEntity, TStatus>(this IRavenQueryable<TEntity> query, object value, QueryOperator filterOperator)
+        where TEntity : ITrackableEntity<TStatus>
+        where TStatus : Enum
+    {
+        if (filterOperator == QueryOperator.In)
+        {
+            var statuses = (List<TStatus>)value;
+            return query.Where(t => t.Status.In(statuses));
+        }
+
+        if (filterOperator == QueryOperator.NotIn)
+        {
+            var statuses = (List<TStatus>)value;
+            return query.Where(t => !t.Status.In(statuses));
+        }
+
+        if (filterOperator == QueryOperator.Equals)
+        {
+            var statuses = GetStatusList();
+            return query.Where(t => t.Status.In(statuses));
+        }
+
+        if (filterOperator == QueryOperator.NotEquals)
+        {
+            var statuses = GetStatusList();
+            return query.Where(t => !t.Status.In(statuses));
+        }
+
+        throw new DomainException($"Operator {filterOperator} is not supported.", default);
+
+        IEnumerable<TStatus> GetStatusList()
+        {
+            return [(TStatus)value];
+        }
+    }
+}
---
+++ GdNetRavenDbExt/RavenDbAsyncUnitOfWork.cs
@@ -0,0 +1,17 @@
+﻿using GdNetDDD.Common;
+using Raven.Client.Documents.Session;
+
+namespace GdNetRavenDbExt;
+
+public class RavenDbAsyncUnitOfWork(IAsyncDocumentSession documentSession) : IUnitOfWork
+{
+    public void Dispose()
+    {
+        // Do not dispose the documentSession, it maybe use in other places within the scope
+    }
+
+    public async Task SaveChangesAsync(CancellationToken cancellationToken = default)
+    {
+        await documentSession.SaveChangesAsync(cancellationToken);
+    }
+}
